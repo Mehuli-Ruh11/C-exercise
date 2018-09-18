21835684c50a91c19140822e5dda5a847b8bee7d
Mehuli-Ruh11 <ruchirapratihar456@gmail.com>
2018-09-18 09:43:31 -0500

    Create postorder_traversal.cpp

---
+++ postorder_traversal.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+private:
+    void postorderTraversal(TreeNode* root, vector<int>& answer) {
+        if (!root) {
+            return;
+        }
+        postorderTraversal(root->left,answer); 
+        postorderTraversal(root->right, answer);   
+        answer.push_back(root->val);               
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> answer;
+        postorderTraversal(root, answer);
+        return answer;
+     
+    }
+
+};
